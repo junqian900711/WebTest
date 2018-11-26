9e9bda8b93a6df9d1079e55aab4aa29d0718f819
junqian900711@gmail.com <1019042603QianJu>
2018-11-26 00:45:10 -0600

    second time

--- WebTestSel.py
+++ WebTestSel.py
@@ -6,6 +6,8 @@ browser.get('http://betastore.carloudy.com')
 browser.maximize_window()
 
 #browser.find_element_by_name()
+
+may not use this next time
 """
 
 from selenium import webdriver
@@ -18,3 +20,5 @@ browser.maximize_window()
 browser.find_element_by_name("username").send_keys("junqianniub")
 browser.find_element_by_name("password").send_keys("1019042603QJ")
 browser.find_element_by_xpath("//button[text()='Log in']").click()
+
+#that is for today
